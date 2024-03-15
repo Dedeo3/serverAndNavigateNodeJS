4dc34baf23460f01de77be7e868492d6dec6e50c
Dedeo3 <93327877+Dedeo3@users.noreply.github.com>
2024-03-15 10:27:59 +0700

    Update server.js

--- server.js
+++ server.js
@@ -60,8 +60,8 @@ const server = http.createServer((req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 8081;
-
+// const PORT = process.env.PORT || 8081;
+const PORT = 8081;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
